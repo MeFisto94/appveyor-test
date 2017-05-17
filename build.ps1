af9166e6e446c6f99d2d132f21c294bb5b03dc15
MeFisto94 <MeFisto94@noreply.users.github.com>
2017-05-17 10:47:30 +0200

    Update Build Script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,9 @@
 . .\degzip.ps1
 (New-Object Net.WebClient).DownloadFile('http://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.gz', 'C:\FT.tar.gz')
-DeGZip-File 'C:\FT.tar.gz'
+# DeGZip-File 'C:\FT.tar.gz'
+# Use 7zip for this...
+PUSHD C:\
+7zip e C:\FT.tar.gz
+7zip x C:\FT.tar
+DEL /F /Q C:\FT.tar FT.tar.gz
+POPD
