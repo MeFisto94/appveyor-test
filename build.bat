bf7a291861154bef7582cc924482d45ebd2e6b1b
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-02-22 00:25:16 +0100

    Fix the Build Environment for AppVeyor / VS2015 / MSYS 1.0

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2015\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+REM C:\MinGW\msys\1.0\msys.bat
+REM Both are wrong: The upper can't enter stuff and the lower misses some environment....
+C:\MinGW\msys\1.0\bin\bash.exe -c "C:\openjdk\build.sh"
 REM C:\msys64\msys2_shell.cmd -mingw64 -c "$APPVEYOR_BUILD_FOLDER/build.sh; exit"
--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ pacman -Sy --noconfirm zip
 
 echo Linking in the Compiler under /tmp/tools. This is because Autoconf does not support paths with spaces
 #ln -s "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/" /tmp/tools
-ln -s "/c/Program Files (x86)/Microsoft Visual Studio/2015/Community/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/" /tmp/tools
+ln -s "/c/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64/" /tmp/tools
 ./configure --prefix=/mingw TOOLS_DIR="/tmp/tools" --with-freetype-include="../../freetype-2.8/include/" --with-freetype-lib="../../freetype-2.8/objs/vc2010/x64"
