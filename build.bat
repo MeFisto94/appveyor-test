64209c4771d723566e452cff71877f5cf1680d74
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-02-21 23:25:57 +0100

    Advance the build further, but this won't help because we're running msys2 but then we get a problem with fixpath. That's why we will now revert to VS2015 and msys 1.0

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-REM C:\msys64\msys2_shell.cmd -c "$APPVEYOR_BUILD_FOLDER/build.sh; exit"
+REM C:\msys64\msys2_shell.cmd -mingw64 -c "$APPVEYOR_BUILD_FOLDER/build.sh; exit"
--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 git clone https://github.com/MeFisto94/openjdk
 cd openjdk
+common/autoconf/autogen.sh
+
+echo "Installing ZIP"
+pacman -Sy --noconfirm zip
+
+echo Linking in the Compiler under /tmp/tools. This is because Autoconf does not support paths with spaces
+ln -s "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/" /tmp/tools
+./configure --prefix=/mingw TOOLS_DIR="/tmp/tools" --with-freetype-include="../../freetype-2.8/include/" --with-freetype-lib="../../freetype-2.8/objs/vc2010/x64"
