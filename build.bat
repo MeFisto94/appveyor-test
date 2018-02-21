2dcbf19063c9abb47b189823ab7fc1c871d6aba0
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-02-21 23:30:29 +0100

    Use VS2015 to have msys. Currently it's guesswork how the folders will be called.

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2015\Community\VC\Auxiliary\Build\vcvars64.bat"
 REM C:\msys64\msys2_shell.cmd -mingw64 -c "$APPVEYOR_BUILD_FOLDER/build.sh; exit"
