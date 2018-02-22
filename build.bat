a22e077c0170a4a96594e5880146e24faed33217
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-02-22 15:19:32 +0100

    Fixes #1 Freetype can now be built, however there are some linker issues remaining

--- build.bat
+++ build.bat
@@ -2,11 +2,20 @@
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+
 echo Building LibFreeType 2.8
-cd C:\freetype2.8\build\
-REM patch < libfreetype.patch
-REM MSBUILD
+pushd C:\freetype-2.8\builds\windows\vc2010\
+patch < C:\openjdk\libfreetype.patch
+MSBuild freetype.sln /property:Platform="x64" /property:Configuration=Release
+MSBuild freetype.sln /property:Platform="x64" /property:Configuration="Release Multithreaded"
+patch < C:\openjdk\libfreetype-2.8-dll.patch
+echo Since App Veyors v100 Buildchain seems damaged, use the Windows 7.1 SDK to build libfreetype
+patch < C:\openjdk\libfreetype-2.8-dll-platform-toolset.patch
+MSBuild freetype.sln /property:Platform="x64" /property:Configuration=Release
+MSBuild freetype.sln /property:Platform="x64" /property:Configuration="Release Multithreaded"
+popd
+
 REM C:\MinGW\msys\1.0\msys.bat
 REM Both are wrong: The upper can't enter stuff and the lower misses some environment....
-C:\MinGW\msys\1.0\bin\bash.exe -c "C:\openjdk\build.sh"
+C:\MinGW\msys\1.0\bin\bash.exe -c "/c/openjdk/build.sh"
 REM C:\msys64\msys2_shell.cmd -mingw64 -c "$APPVEYOR_BUILD_FOLDER/build.sh; exit"
--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@ git clone https://github.com/MeFisto94/openjdk
 cd openjdk
 common/autoconf/autogen.sh
 
-echo "Installing ZIP"
-pacman -Sy --noconfirm zip
+#Only required on msys2:, but msys2 fails the fixpath.c compilation anyway as of now.
+#echo "Installing ZIP"
+#pacman -Sy --noconfirm zip
 
 echo Linking in the Compiler under /tmp/tools. This is because Autoconf does not support paths with spaces
 #ln -s "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/" /tmp/tools
