5e5e75d98e41112875ab3143efbdc0a21e832beb
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-02-22 14:20:18 +0100

    Start building libfreetype

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
+@ECHO OFF
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+echo Building LibFreeType 2.8
+cd C:\freetype2.8\build\
+REM patch < libfreetype.patch
+REM MSBUILD
 REM C:\MinGW\msys\1.0\msys.bat
 REM Both are wrong: The upper can't enter stuff and the lower misses some environment....
 C:\MinGW\msys\1.0\bin\bash.exe -c "C:\openjdk\build.sh"
