567d1e1fbdf7b29da83e589a66e4a80894711eb1
MeFisto94 <MeFisto94@users.noreply.github.com>
2019-10-03 15:52:50 +0200

    Finally fix #3 : The openJDK Repository needed an upgraded libraries.m4 which links with "legacy_stdio_definitions.lib". Also the path was incorrect now.

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ MSYSTEM=MINGW32
 echo Linking in the Compiler under /tmp/tools. This is because Autoconf does not support paths with spaces
 #ln -s "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/" /tmp/tools
 ln -s "/c/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64/" /tmp/tools
-./configure --prefix=/mingw TOOLS_DIR="/tmp/tools" --with-freetype-include="../../freetype-2.8/include/" --with-freetype-lib="../../freetype-2.8/objs/vc2010/x64"
+./configure --prefix=/mingw TOOLS_DIR="/tmp/tools" --with-freetype-include="../../freetype-2.8/include/" --with-freetype-lib="../../freetype-2.8/objs/win64/vc2010"
